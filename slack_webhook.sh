448fb1d0cde36c89d9d78dbdbf56b0b8996490f9
payam <saderi@gmail.com>
2018-11-01 20:03:41 +0300

    Get message content from variable

--- slack_webhook.sh
+++ slack_webhook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-function slack_webhook {
-    SLACK_WEBHOOK_MESSAGE="Disk Usage Warning. Disk used over $USED%"
+function slack_webhook  {
+    SLACK_WEBHOOK_MESSAGE=$1
     curl -X POST \
     --data-urlencode "payload={\"channel\": \"$SLACK_WEBHOOK_CHANNEL\", \"text\": \"$SLACK_WEBHOOK_MESSAGE\"}" \
     $SLACK_WEBHOOK_URL
